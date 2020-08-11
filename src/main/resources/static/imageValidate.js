136b3d252b02e54aa92356adf51f336a16a8fb82
heshengjin <2356899074@qq.com>
2020-08-11 18:12:19 +0800

    滑块验证

--- src/main/resources/static/imageValidate.js
+++ src/main/resources/static/imageValidate.js
@@ -25,15 +25,30 @@ $(function(){
         cbEnd:function(ev){//移动结束时候的回调函数
             left_binarydance = 67;
             console.log(ev.clientX - 67 == positionX ? 'true' : 'false');
+            //失败回到原地
             if(!(ev.clientX - 67 == positionX)){
                 $("#sliderInner-capture-bd").css("left",(left_binarydance-67)+"px");
                 $("#slideImage-capture-bd").css("left",(left_binarydance-67)+"px");
+                $("#container-capture-bd").shake(2, 10, 400);
             }
             //checkImageValidate();
         }
     });
 
 });
+//$("抖动元素").shake(次数, 距离, 持续时间);
+jQuery.fn.shake = function (intShakes /*Amount of shakes*/, intDistance /*Shake distance*/, intDuration /*Time duration*/) {
+    this.each(function () {
+        var jqNode = $(this);
+        jqNode.css({ position: 'relative' });
+        for (var x = 1; x <= intShakes; x++) {
+            jqNode.animate({ left: (intDistance * -1) }, (((intDuration / intShakes) / 4)))
+            .animate({ left: intDistance }, ((intDuration / intShakes) / 2))
+            .animate({ left: 0 }, (((intDuration / intShakes) / 4)));
+        }
+    });
+    return this;
+}
 
 function initImageValidate(){
     $.ajax({
